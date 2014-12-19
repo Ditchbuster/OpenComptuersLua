9d6993293bec3e91addb59582da9a024f222c60f
Ditchbuster <trioman008@hotmail.com>
2014-12-18 23:09:18 -0600

    digBox() now works properly
    properly tracks facing and abs coord from starting orientation. Changed
    var labels to be consistent with commented axis and directions

--- RobotTest.lua
+++ RobotTest.lua
@@ -4,13 +4,13 @@ local rb = require("robot")
 local absX,absY,absZ = 0,0,0 -- these are the coords of the bot reletive to the starting point, X is in the +north -south direction, Y in the +east -west direction and Z in the +up -down direction
 local facing = 0 -- facing relative to starting direction. north is forward (0), east is right (1), south is back (2), west is left(3) from the staring position
 
-function digBox( maxY , maxX , maxZ )
+function digBox( maxX , maxY , maxZ )
     rb.swing()
     forward() --moves in to the area that you want hollowed out
     turnRight() -- aligns robot from my starting position of front left of area to be dug
-    for y=0,maxY do
+    for x=0,maxX do
         for z =0,maxZ do
-            for i=0,maxX-1 do
+            for y=0,maxY-1 do
                 if rb.durability()==nil then
                     return "No Pickaxe"
                 end
@@ -20,42 +20,39 @@ function digBox( maxY , maxX , maxZ )
                     if move[2]=='entity' then
                         repeat
                             os.sleep(100)
-                            until rb.detect()==false
-                            else
-                                return move[2]
-                            end
-                        end
-                        print("@".. i .."," .. y .. "," .. z)
-                    end
-                    if z ~=maxZ then
-                        if y%2==1 then
-                            rb.swingUp()
-                            up()
-                        else
-                            rb.swingDown()
-                            down()
-                        end
-
-                        turnRight()
-                        turnRight()
-                    end
-                end
-                if y ~= maxY then
-                    if y%2==1 then
-                        turnLeft()
-                        rb.swing()
-                        forward()
-                        turnLeft()
+                        until rb.detect()==false
                     else
-                        turnRight()
-                        rb.swing()
-                        forward()
-                        turnRight()
+                        return move[2]
                     end
                 end
+                print("@".. absX .."," .. absY .. "," .. absZ)
             end
-            return "Done"
+            if z ~=maxZ then
+                if x%2==0 then
+                    rb.swingUp()
+                    up()
+                else
+                    rb.swingDown()
+                    down()
+                end
+                turnRight()
+                turnRight()
+            end
         end
+        if x ~= maxX then --if not the last row then move up a row (+X direction)
+            local tmpFacing=facing --store direction just came from along Y
+            turn(0) --turn north
+            rb.swing() --swing pickaxe ******** TODO: needs durability check
+            forward()
+            if tmpFacing==1 then  -- then turn to opposite direction then it had been before moving north
+                turn(3)
+            else
+                turn(1)
+            end         
+        end
+    end
+    return "Done"
+end
 function forward() --adjusts the abs vars appropreatly and returns robot.forward() return
     if facing==0 then
         absX=absX+1
@@ -113,5 +110,5 @@ end
 print(facing)
 turn(2)
 print(facing)
---print(digBox(3,3,3))
+print(digBox(3,3,3))
 print(absX.." "..absY.." "..absZ)
