229f3bae2b8e4a4d1afce8e17b8892f57a22d669
Ditchbuster <trioman008@hotmail.com>
2014-12-18 18:35:08 -0600

    facing and coord tracking
    added in functions to keep track of distance and facing from starting
    point.

--- RobotTest.lua
+++ RobotTest.lua
@@ -1,11 +1,13 @@
 --local component = require("component")
 --local sd = require("sides")
-rb = require("robot")
+local rb = require("robot")
+local absX,absY,absZ = 0,0,0 -- these are the coords of the bot reletive to the starting point, X is in the +north -south direction, Y in the +east -west direction and Z in the +up -down direction
+local facing = 0 -- facing relative to starting direction. north is forward (0), east is right (1), south is back (2), west is left(3) from the staring position
 
 function digBox( maxY , maxX , maxZ )
     rb.swing()
-    rb.forward() --moves in to the area that you want hollowed out
-    rb.turnRight() -- aligns robot from my starting position of front left of area to be dug
+    forward() --moves in to the area that you want hollowed out
+    turnRight() -- aligns robot from my starting position of front left of area to be dug
     for y=0,maxY do
         for z =0,maxZ do
             for i=0,maxX-1 do
@@ -13,46 +15,103 @@ function digBox( maxY , maxX , maxZ )
                     return "No Pickaxe"
                 end
                 rb.swing()
-                local move = {rb.forward()}
+                local move = {forward()}
                 if move[1]==nil then
                     if move[2]=='entity' then
                         repeat
                             os.sleep(100)
-                        until rb.detect()==false
-                    else
-                        return move[2]
+                            until rb.detect()==false
+                            else
+                                return move[2]
+                            end
+                        end
+                        print("@".. i .."," .. y .. "," .. z)
+                    end
+                    if z ~=maxZ then
+                        if y%2==1 then
+                            rb.swingUp()
+                            up()
+                        else
+                            rb.swingDown()
+                            down()
+                        end
+
+                        turnRight()
+                        turnRight()
                     end
                 end
-                print("@".. i .."," .. y .. "," .. z)
-            end
-            if z ~=maxZ then
-                if y%2==0 then
-                    rb.swingUp()
-                    rb.up()
-                else
-                    rb.swingDown()
-                    rb.down()
+                if y ~= maxY then
+                    if y%2==1 then
+                        turnLeft()
+                        rb.swing()
+                        forward()
+                        turnLeft()
+                    else
+                        turnRight()
+                        rb.swing()
+                        forward()
+                        turnRight()
+                    end
                 end
-
-                rb.turnAround()
             end
+            return "Done"
         end
-        if y ~= maxY then
-            if y%2==1 then
-                rb.turnLeft()
-                rb.swing()
-                rb.forward()
-                rb.turnLeft()
-            else
-                rb.turnRight()
-                rb.swing()
-                rb.forward()
-                rb.turnRight()
-            end
-        end
+function forward() --adjusts the abs vars appropreatly and returns robot.forward() return
+    if facing==0 then
+        absX=absX+1
+    elseif facing ==1 then
+        absY=absY+1
+    elseif facing ==2 then
+        absX=absX-1
+    elseif facing ==3 then
+        absY=absY-1
     end
-    return "Done"
+    return rb.forward()
 end
+function up() -- adjusts the absX for up and down
+    absZ=absZ+1
+    return rb.up()
+end
+function down()
+    absZ=absZ-1
+    return rb.down()
+end
+function turnRight() --adj facing var and turns
+    if facing==3 then
+        facing=0
+    else
+        facing=facing+1
+    end
+    return rb.turnRight()
+end
+function turnLeft()
+    if facing ==0 then
+        facing=3
+    else
+        facing=facing-1
+    end
+    return rb.turnLeft()
+end
+function turn(desFacing) -- no one seems to know if robot.turnLeft/Right() returns anything or not... 
+    if type(desFacing)~="number" then error ("Turn desired should be a number") end
+    if desFacing>3 or desFacing<0 then error("Turn desired between 0 and 3") end
 
+    local tn = desFacing-facing
+    if tn== 1 or tn==-3 then
+        return turnRight()
+    elseif tn==-2 or tn == 2 then
+        turnRight()
+        turnRight()
+        return --until i find doc or code showing if robot.turnRight() returns anything i am just not returning anything..
 
-print(digBox(3,3,3))
+    elseif tn == -1 or tn == 3 then
+        return turnLeft()
+    else
+        return nil,"No turn desired"
+    end
+end
+print(facing)
+turn(2)
+print(facing)
+--print(digBox(3,3,3))
+print(absX.." "..absY.." "..absZ)
